c526567e91844e25cc832e4fce1d449af90201b2
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-26 10:48:53 -0500

    finished search saga

--- src/redux/sagas/search.saga.js
+++ src/redux/sagas/search.saga.js
@@ -3,7 +3,9 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* searchCards(action) {
     try {
-        yield axios.get('/api/user/search', action.payload);
+        console.log(action.payload)
+        const searchResponse = yield axios.get('/api/user/search', action.payload);
+        yield put ({type: 'SET_SEARCH', payload: searchResponse.data});
     }
     catch (error) {
         console.log('error in search', error)
