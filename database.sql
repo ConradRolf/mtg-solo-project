3a6c37dc818fe98d9955ec96e62ec4cfdedb5f8b
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-26 10:43:05 -0500

    set up database

--- database.sql
+++ database.sql
@@ -8,3 +8,21 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "deck" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "name" VARCHAR(255) DEFAULT "My Deck"
+);
+
+CREATE TABLE "card" (
+    "id" SERIAL PRIMARY KEY,
+    "deck_id" INT REFERENCES "deck" NOT NULL,
+    "card_id" INT,
+    "img" VARCHAR(2000),
+    "card_name" VARCHAR(500),
+    "cmc" INT,
+    "color_identity" VARCHAR(255),
+    "description" TEXT,
+    "rulings" TEXT
+);
